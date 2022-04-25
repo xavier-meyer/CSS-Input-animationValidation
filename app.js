3387547e8f08d107e3089e3030a457229bdb2946
xavier-meyer <xavier.01@orange.fr>
2022-04-25 10:47:52 +0200

    feat: fct JS controle saisie user

--- app.js
+++ app.js
@@ -5,7 +5,48 @@ const validation = document.querySelector(".inp-wrap span");
 
 //on mets une écoute d"evenement sur l'input de type input. On déclenches une fonction. e => objet d'évenement qui regroupe les propriétés contenus ds le input.
 
+
 input.addEventListener("input", function(e){
+
+    input.addEventListener("keydown", (event)=>{
+     // interdire certains caractéres à la saisie ds le input et prévenir l'utilisateur
+     let arrayBadLetters = [
+        "$",
+        "!",
+        "?",
+        "%",
+        "+",
+        "-",
+        "/",
+        "*",
+        ";",
+        "=" 
+    ];
+    if(arrayBadLetters.includes(event.key)){
+        event.preventDefault();
+        console.log("erreur!");
+        input.classList.add("erreur");
+        validation.textContent = "Les caractéres spéciaux sont interdits.";
+        validation.classList.add("visible");
+        setTimeout(()=>{
+            validation.classList.remove("visible");
+            input.classList.remove("erreur");
+            }, 4000)
+    }
+    // prévenir que le champ est vide
+    else if(e.target.value == ""){
+        input.classList.add("erreur");
+        validation.textContent = "Veuillez entrer un mot dans le champ de saisie!";
+        validation.classList.add("visible");   
+        setTimeout(()=>{
+            validation.classList.remove("visible");
+            input.classList.remove("erreur");
+            }, 4000)
+        } else {
+        input.classList.remove("erreur");
+        validation.classList.remove("visible"); 
+        }
+    })    
     // affiche l'objet d'évenement + toutes les propriétés
     // console.log(e);
 
@@ -19,5 +60,4 @@ input.addEventListener("input", function(e){
     } else if (e.target.value === ""){
         e.target.parentNode.classList.remove("active-input");
     }
-
-}) 
+})
